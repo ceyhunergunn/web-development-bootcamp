1ee660325dfcdbc2fcf51dbce4dc41a83a1020d4
Ceyhun Ergün <ceyhunergun61@gmail.com>
2022-11-21 01:18:18 +0000

    All functions added.

--- Yukatech Challange/app.js
+++ Yukatech Challange/app.js
@@ -1,8 +1,11 @@
+// Gerekli ID'ler çağırıldı.
+
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputText = document.getElementById('inputText');
 let clearToDo = document.getElementById('clearToDo');
 
+// ADD butonuna tek tıklama ile gerçekleştirelecek işlem ayarlandı.
 addToDoButton.addEventListener('click', function(){
     let paragraph = document.createElement('p');
     paragraph.classList.add('paragraph');
@@ -10,15 +13,20 @@ addToDoButton.addEventListener('click', function(){
     paragraph.innerHTML = inputText.value;
     inputText.value= "";
 
+    //Eklenen aktivite üzerine tek tıkla üst çizme ve renk değişme işlemi yapıldı.
     paragraph.addEventListener('click', function () {
         paragraph.style.textDecoration = 'line-through';
         paragraph.style.color = 'green';
     })
 
-
+    //Eklenen aktivite üzerine çift tıkla silme işlemi yapıldı.
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
     })
+
+    //CLEAR butonuna tek tıklama ile silme işlemi yapıldı.
+        //display = none ile tamamen silinmedi.
+        //ekranda gizlendi.
     clearToDo.addEventListener('click', function(){
         paragraph.style.display= 'none';
     })
--- Yukatech Challange/validation.js
+++ Yukatech Challange/validation.js
@@ -1,2 +1,11 @@
+//Formdan gerekli ID'ler çağırıldı.
 var email= document.forms['form']['email'];
-var email= document.forms['form']['password'];
+var password= document.forms['form']['password'];
+
+//Değerlendirme işlemi yapıldı.
+function validated(){
+    if ((email.value == 'yuka@yuka.com') && (password.value == 'yuka123')){
+        return true;  
+    }
+    else{return false};
+}
