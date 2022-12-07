d7ffb729017733e3f5de7fbb1a9abf53bc599847
Ceyhun Ergün <ceyhunergun61@gmail.com>
2022-12-07 11:57:28 +0000

    Chapter 18: Node.js is completed.

---
+++ Node.js/index.js
@@ -0,0 +1,16 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt","file2.txt")
+
+const superheroes = require("superheroes");
+ 
+var mySuperHeroName = superheroes.random();
+
+console.log("My Super Hero is "+mySuperHeroName);
+
+
+const supervillains = require('supervillains');
+
+var mySuperVillains = supervillains.random();
+
+console.log("My Super Villains is "+mySuperVillains);
