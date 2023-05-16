9936eb4c73c86a0796688587f295e9f4efbcb883
Ceyhun Ergün <ceyhunergun61@gmail.com>
2023-05-16 20:45:37 +0000

    Chapter 33 ReactJS %55 completed.

--- ReactJS/keeper-app/src/App.jsx
+++ ReactJS/keeper-app/src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Header from './Header'
-import Note from './Note'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
-        <Header/>
-        <Note/>
-        <Footer/>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
